cbd919a69af5348dabb68735bea70aa44523fe1d
Warren Clough <Warren Clough>
2016-04-13 03:54:26 -0500

    Fixed  bug in user model

--- app/User.php
+++ app/User.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
@@ -31,19 +32,4 @@ class User extends Authenticatable
         $this->attributes['password'] = bcrypt($password);
     }
 
-    public static function boot()
-    {
-        parent::boot();
-
-        static::creating(function($post)
-        {
-            $post->created_by = Auth::user()->id;
-            $post->updated_by = Auth::user()->id;
-        });
-
-        static::updating(function($post)
-        {
-            $post->updated_by = Auth::user()->id;
-        });
-    }
 }
